bd9e4e36eba08d87d0d89e8c00c26a9db60d1319
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-06-15 15:08:12 +0200

    Active class added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,7 @@
 <!-- Header -->
 
 <header class="mb-5">
+  {{ Route::currentRouteName()}}
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="{{ route('home') }}">COMIC BOOK STORE</a>
@@ -10,7 +11,12 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link" href="{{ route('home') }}">HOME</a>
+                <a class="nav-link  @if(Route::currentRouteName() == 'home') active @endif"
+                href="{{ route('home') }}">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link  @if(Route::currentRouteName() == 'comics.index') active @endif"
+                href="{{ route('comics.index') }}">COMICS</a>
               </li>
             </ul>
           </div>
